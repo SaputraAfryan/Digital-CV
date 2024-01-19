b603c8f2bd9a83c30bfe2b9cd95ce4cf16a916af
Muhammad Afryan Saputra <afryan54@gmail.com>
2024-01-19 17:13:15 +0000

    modified:   .streamlit/config.toml 	modified:   Digital_CV.py 	modified:   assets/css/main.css

--- Digital_CV.py
+++ Digital_CV.py
@@ -66,11 +66,9 @@ with open(PATH_CSS) as f:
     st.markdown("<style>{}</style>".format(f.read()), unsafe_allow_html=True)
 profile_pic = Image.open(f"{PATH_IMAGES}/profile-pic.png")
 
-arrow_icon = """
-<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-left-circle-fill" viewBox="0 0 16 16">
+arrow_icon = '''<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-left-circle-fill" viewBox="0 0 16 16">
   <path d="M8 0a8 8 0 1 0 0 16A8 8 0 0 0 8 0m3.5 7.5a.5.5 0 0 1 0 1H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5z"></path>
-</svg>
-"""
+</svg>'''
 
 # --- SIDE BAR ---
 with st.sidebar:
@@ -78,7 +76,7 @@ with st.sidebar:
     with scol1:
         st.image(profile_pic, width=80)
     with scol2:
-        st.write("## Muhammad Afryan Saputra's")
+        st.markdown('''<h2> Muhammad Afryan Saputra's</h2>''', unsafe_allow_html=True)
     st.write("---")
     st.markdown('''
                 <ul style="list-style-type:none;">
@@ -104,7 +102,7 @@ with st.sidebar:
                     </li>
                     <li>
                         <button class="sidebar-button">
-                            <a href="#workexperience" target="_self" class="sidebar-link">
+                            <a href="#work-experience" target="_self" class="sidebar-link">
                                 Work Experience
                             </a>
                         </button>
@@ -117,17 +115,24 @@ with st.sidebar:
                         </button>
                     </li>
                 </ul>
+
+                <script>
+                    document.body.scrollTop = 0; // For Safari
+                    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+                </script>
                 '''
     , unsafe_allow_html=True)
 
 # --- MAIN PAGE ---
-st.markdown("<a id='top'></a>", unsafe_allow_html=True)
-col1, col2 = st.columns([1, 2])
 with st.container():
+    st.markdown('''<a name="top"></a>''',unsafe_allow_html=True)
+
+with st.container():
+    col1, col2 = st.columns([1, 2])
     with col1:
-        st.image(profile_pic, width=250)
+        st.image(profile_pic, width=250, use_column_width=True)
     with col2:
-        st.write("# Muhammad Afryan Saputra")
+        st.markdown('''<h1 id="fullname"> Muhammad Afryan Saputra</h1>''', unsafe_allow_html=True)
         st.write(f"**{INFO_GEN.address}**")
         st.write(f"🎂 : **{INFO_GEN.birth}**")
         st.write(f"📧 : **{INFO_GEN.email}**")
@@ -144,7 +149,7 @@ for i, (platform, link) in enumerate(SOCIAL_MEDIA.items()):
 # --- SUMMARY ---
 with st.container():
     st.write("---")
-    st.markdown("<h1 id='summary'>Summary</h1>", unsafe_allow_html=True)
+    st.markdown('''<h1 id='summary'>Summary</h1>''', unsafe_allow_html=True)
     st.write('''
     Bachelor's degree in Informatics Engineering at Telkom University with a Data Science concentration and `240+` hours of internship experience at UPTD PSDA WS Cisadea-Cibareno, `500+` hours working on projects in data science and data analysis throughout 2023.
 ''')
@@ -170,7 +175,7 @@ def s_cols(x: dict):
 # --- EDUCATION ---
 with st.container():
     st.write("#")
-    st.markdown("<h1 id='education'>Education</h1>", unsafe_allow_html=True)
+    st.markdown('''<h1 id="""education""">Education</h1>''', unsafe_allow_html=True)
     col_text("**Bachelor of Informatics Engineering** (Informatics Engineering), *Telkom University*, Indonesia", "2019-2023")
     st.write('''
     - GPA: `3.04`
@@ -183,7 +188,7 @@ with st.container():
 # --- WORK EXPERIENCES ---
 with st.container():
     st.write("#")
-    st.write("<h1 id='workexperience'>Work Experience</h1>", unsafe_allow_html=True)
+    st.markdown('''<h1 id="work-experience">Work Experience</h1>''', unsafe_allow_html=True)
     col_text("**Mobile Application Developer**, UPTD PSDA WS Cisadea-Cibareno, Indonesia | Internship", "07/2022 - 09/2022")
     st.write('''
 - Utilized Python scripting to efficiently organize and process employee data, resulting in a `22%` reduction in data processing time
@@ -201,7 +206,7 @@ with st.container():
      
 # --- SKILLS ---
     st.write("#")
-    st.markdown("<h1 id='skills'>Skills</h1>", unsafe_allow_html=True)
+    st.markdown('''<h1 id="skills">Skills</h1>''', unsafe_allow_html=True)
     s_cols(SKILLS)
 
 
